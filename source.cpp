0a35de5460d61b6d219fc5dceb175c259dc9215f
OzoNeTT <gpoff12@mail.ru>
2019-09-23 18:58:23 +0300

    Done

--- source.cpp
+++ source.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <string>
 #include <random>
+#include <iomanip>
 
 double myFunction(double x) {
     return -sqrt(x)*sin(x);
@@ -11,16 +12,42 @@ double myFunction(double x) {
 double multiModalFunction(double x){
     return myFunction(x)*sin(5 * x);
 }
+void PrintN(double (&N_storage)[20][10], int q_size, int p_size){
+    for(int raws = 0; raws < q_size; raws++) {
+        for (int columnes = 0; columnes < p_size; columnes++) {
+            std::cout << N_storage[raws][columnes] << "\t|\t";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n\n";
+}
 
-void RandomSearch(double a, double b) {
+void PrintResultUnimodal(double (&X_storage)[20][10], int q_size, int p_size) {
+    for(int raws = 0; raws < q_size; raws++) {
+        for (int columnes = 0; columnes < p_size; columnes++) {
+            std::cout << X_storage[raws][columnes] << "\t|\t";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n\n";
+}
+
+void PrintResultMultimodal(double (&X_storage)[20][10], int q_size, int p_size) {
+    for(int raws = 0; raws < q_size; raws++) {
+        for (int columnes = 0; columnes < p_size; columnes++) {
+            std::cout << std::fixed << std::setprecision(6)
+                      << multiModalFunction(X_storage[raws][columnes]) << "\t|\t";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n\n";
+}
+
+void RandomSearch(double begin, double end, std::string function) {
 
     std::vector<double> propabilities;
     std::vector<double> q;
-    double propability = 0.9;
-    double firs_q = 0.005;
-    double current_N;
-    double best_y = 0;
-    double best_x = 0;
+    double propability = 0.9, firs_q = 0.005, current_N, best_y = 0, best_x = 0;
     double N_storage[20][10];
     double X_storage[20][10];
     std::vector<double> current_x_storage;
@@ -35,53 +62,76 @@ void RandomSearch(double a, double b) {
         firs_q += 0.005;
     }
 
-    for(int i = 0; i < (int)q.size(); i++){
-        for (int j = 0; j < (int)propabilities.size(); j++) {
+    for(int q_iterator = 0; q_iterator < (int)q.size(); q_iterator++){
+        for (int p_iterator = 0; p_iterator < (int)propabilities.size(); p_iterator++) {
             best_x = 0;
             best_y = 0;
-            N_storage[i][j] = log(1 - propabilities[j]) / log(1 - q[i]);
-            current_N = N_storage[i][j];
+            N_storage[q_iterator][p_iterator] = std::ceil(log(1 - propabilities[p_iterator]) / log(1 - q[q_iterator]));
+            current_N = N_storage[q_iterator][p_iterator];
 
-            for(int k = 0; k < current_N; k++) {
-                current_x_storage.push_back(a + (double)(rand())/RAND_MAX*(b - a) + a);
+            for(int index = 0; index < current_N; index++) {
+                current_x_storage.push_back(begin + (double)(rand())/RAND_MAX*(end - begin) + begin);
             }
 
-            for(int k = 0; k < current_x_storage.size(); k++) {
-                if(myFunction(current_x_storage[k]) < best_y) {
-                    best_y = myFunction(current_x_storage[k]);
-                    best_x = current_x_storage[k];
+            if(function == "unimodal") {
+                for (int index = 0; index < current_x_storage.size(); index++) {
+                    if (myFunction(current_x_storage[index]) < best_y) {
+                        best_y = myFunction(current_x_storage[index]);
+                        best_x = current_x_storage[index];
+                    }
                 }
             }
-            
-            //for multimodal
-            //for(int k = 0; k < current_x_storage.size(); k++) {
-            //    if (fabs(multiModalFunction(current_x_storage[k])) > best_y) {
-            //        best_y = fabs(myFunction(current_x_storage[k]));
-            //        best_x = current_x_storage[k];
-            //    }
-            //}
+            else if(function == "multimodal") {
+                for(int index = 0; index < current_x_storage.size(); index++) {
+                    if (fabs(multiModalFunction(current_x_storage[index])) > best_y) {
+                        best_y = fabs(myFunction(current_x_storage[index]));
+                        best_x = current_x_storage[index];
+                    }
+                }
+            }
 
-            X_storage[i][j] = best_x;
+            X_storage[q_iterator][p_iterator] = best_x;
             current_x_storage.clear();
         }
     }
-
-    for(int i = 0; i < q.size(); i++) {
-        for (int j = 0; j < propabilities.size(); j++) {
-            std::cout << X_storage[i][j] << "   ";
-            //for multimodal     std::cout << multiModalFunction(X_storage[i][j]) << "   ";
-        }
-        std::cout << "\n";
+    if(function == "unimodal") {
+        PrintN(N_storage, q.size(), propabilities.size());
+        PrintResultUnimodal(X_storage, q.size(), propabilities.size());
     }
-
+    else if(function == "multimodal")
+        PrintResultMultimodal(X_storage, q.size(), propabilities.size());
     std::cout << std::endl;
 
 
 }
 int main() {
     srand(time(nullptr));
-    double begin = 0, end = 3;
-    RandomSearch(begin, end);
+    double begin, end;
+    std::string userChoice;
+    std::cout << "Hello, user! This is test app =)\n";
+    while(userChoice != "n") {
+        std::cout << "\tEnter value \'a\'\n>> ";
 
+        while(!(std::cin >> begin)) {
+            std::cin.clear();
+            std::cin.ignore(100, '\n');
+            std::cout << "Not a value, try again\n>> ";
+        }
+        std::cout << "Enter value \'b\'\n>> ";
+        while(!(std::cin >> end)) {
+            std::cin.clear();
+            std::cin.ignore(100, '\n');
+            std::cout << "Not a value, try again\n>> ";
+        }
+
+        RandomSearch(begin, end, "unimodal");
+        RandomSearch(begin, end, "multimodal");
+        userChoice = "";
+        while(userChoice != "n" && userChoice !="y") {
+            std::cout << "\nWant again? (y/n): ";
+            std::cin.clear();
+            std::cin >> userChoice;
+        }
+    }
     return 0;
 }
