a8f5143c4de9656f64d21d2e84fad45c0404226e
OzoNeTT <gpoff12@mail.ru>
2019-09-23 17:06:09 +0300

    Done, but govno =)

--- source.cpp
+++ source.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cmath>
 #include <string>
+#include <random>
 
 double myFunction(double x) {
     return -sqrt(x)*sin(x);
@@ -11,15 +12,18 @@ double multiModalFunction(double x){
     return myFunction(x)*sin(5 * x);
 }
 
-void RandomSearch(double begin, double end) {
+void RandomSearch(double a, double b) {
+
     std::vector<double> propabilities;
     std::vector<double> q;
     double propability = 0.9;
     double firs_q = 0.005;
-    double epsilon;
-    const int raws = 20;
-    const int col = 10;
-    double epsilons[raws][col];
+    double current_N;
+    double best_y = 0;
+    double best_x = 0;
+    double N_storage[20][10];
+    double X_storage[20][10];
+    std::vector<double> current_x_storage;
 
     for (int i = 0; i < 10; i++) {
         propabilities.push_back(propability);
@@ -31,17 +35,42 @@ void RandomSearch(double begin, double end) {
         firs_q += 0.005;
     }
 
-    for(int i = 0; i < propabilities.size(); i++){
-        for (int j = 0; j < q.size(); j++) {
-            epsilon = (end - begin) * q[j];
+    for(int i = 0; i < (int)q.size(); i++){
+        for (int j = 0; j < (int)propabilities.size(); j++) {
+            best_x = 0;
+            best_y = 0;
+            N_storage[i][j] = log(1 - propabilities[j]) / log(1 - q[i]);
+            current_N = N_storage[i][j];
+
+            for(int k = 0; k < current_N; k++) {
+                current_x_storage.push_back(a + (double)(rand())/RAND_MAX*(b - a) + a);
+            }
+
+            for(int k = 0; k < current_x_storage.size(); k++) {
+                if(myFunction(current_x_storage[k]) < best_y) {
+                    best_y = myFunction(current_x_storage[k]);
+                    best_x = current_x_storage[k];
+                }
+            }
+            X_storage[i][j] = best_x;
+            current_x_storage.clear();
         }
     }
+
+    for(int i = 0; i < q.size(); i++) {
+        for (int j = 0; j < propabilities.size(); j++) {
+            std::cout << X_storage[i][j] << "   ";
+        }
+        std::cout << "\n";
+    }
+
     std::cout << std::endl;
 
 
 }
 int main() {
-    double begin, end;
+    srand(time(nullptr));
+    double begin = 0, end = 3;
     RandomSearch(begin, end);
 
     return 0;
